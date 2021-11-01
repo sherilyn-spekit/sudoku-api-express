2f9c1d064e7d1a3ccac3806f692bf8504d35f3b0
Sherilyn Villareal <sherilynvillareal@Sherilyns-MacBook-Pro.local>
2021-11-01 17:49:17 -0600

    initial commit

---
+++ routes/generate.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+var sudoku = require('../sudoku-js/sudoku');
+
+
+/* GET generated sudoku puzzle and solution . */
+router.get('/', function(req, res, next) {
+
+  //let difficulty = req.query.difficulty;
+  let difficulty;
+  //if (!(req.query.difficulty == 'medium') || (req.query.difficulty == 'hard') ) {
+  if (req.query.difficulty != 'hard' && req.query.difficulty != 'medium' ) {
+    difficulty = 'easy';
+  } else {
+    difficulty = req.query.difficulty;
+  }
+  console.log(difficulty);
+  let puzzle = sudoku.generate(difficulty);
+  let solvedPuzzle = sudoku.solve(puzzle);
+  
+  solvedPuzzle = sudoku.solve(puzzle);
+  
+  let data = {
+    'difficulty': difficulty,
+    'puzzle': puzzle,
+    'solution': solvedPuzzle
+  };
+    res.json(data);
+});
+
+module.exports = router;
---
+++ sudoku-js/README.md
@@ -0,0 +1,11 @@
+sudoku.js
+=========
+
+Sudoku Javascript library, based on Peter Norvig's solver
+
+This library was extracted from the Sudoku games at https://cardgames.io/sudoku/ and https://sudokuu.com.
+
+It contains functions to generate and solve puzzles, get a list of conflicts in 
+ongoing puzzles, and serialize and deserialize puzzles.
+
+For a more comprehensive description of the library look at http://einaregilsson.com/sudoku-in-javascript
