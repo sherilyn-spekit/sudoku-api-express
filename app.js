5bf4790becdd3742e3c424f2c44b3fb8917da435
Sherilyn Villareal <sherilynvillareal@Sherilyns-MacBook-Pro.local>
2021-11-16 13:44:08 -0700

    updated cors policy

--- app.js
+++ app.js
@@ -19,6 +19,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Add headers before the routes are defined
+app.use(function (req, res) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+  // // Request headers you wish to allow
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // // Set to true if you need the website to include cookies in the requests sent
+  // // to the API (e.g. in case you use sessions)
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // // Pass to next layer of middleware
+  // next();
+});
+
 app.use('/', indexRouter);
 app.use('/generate', generateRouter);
 
